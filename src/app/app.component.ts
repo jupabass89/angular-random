19e736486df418a5bfc5017e32ba2c9c0a81f613
jupabass89 <jupabass89@gmail.com>
2019-10-20 22:29:37 -0500

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'dojo';
+  number = 0;
+
+emitRandom($event) {
+  this.number = $event;
 }
+
+}
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmiterComponent } from './components/emiter/emiter.component';
+import { ReciverComponent } from './components/reciver/reciver.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmiterComponent,
+    ReciverComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
